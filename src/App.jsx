cb3dceaaaafa5f236902cdbb82c84ef49cb0a8bd
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-02-28 10:51:39 +0200

    In 1.2 Content is added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Content from "./components/Content"
 
 const App = () => {
     const course = 'Half Stack application development'
@@ -12,6 +13,12 @@ const App = () => {
     return (
       <>
         <Header course = {course} />
+        <Content 
+            part1 = {part1} exercises1 = {exercises1}
+            part2 = {part2} exercises2 = {exercises2}
+            part3 = {part3} exercises3 = {exercises3}
+
+        />
       </>
     )
   }
