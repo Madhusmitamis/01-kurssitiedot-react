0ddc87c86f15cb19ab33fa495c8771cfbfe821e9
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-02-28 12:25:29 +0200

    1.4 completed.

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,27 @@ import Total from "./components/Total"
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
+    const parts =[ {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-    const part2 = {
+    },
+     {
       name: 'Using props to pass data',
       exercises: 7
-    }
-    const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
-    }
+    }]
     return (
       <>
         <Header course = {course} />
         <Content 
-            part1 = {part1}
-            part2 = {part2}
-            part3 = {part3}
+            parts = {parts}
 
         />
         <Total 
-        part1 = {part1}
-        part2 = {part2}
-        part3 = {part3}
+        parts = {parts}
 
 
         />
